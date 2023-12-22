4b854cbad7d1cc2ced25a71bebea402d2f89d05d
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-12-22 20:16:44 +0100

    generate a sprin boot web application

---
+++ src/main/java/ma/youcode/myrh/MyrhApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.myrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/youcode/myrh/MyrhApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.myrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
